cfcafb45f91bfc0a199cccc33e411b82d8abb411
Alvaro <alvarof.alves@hotmail.com>
2018-11-06 16:17:05 -0200

    update Jquery

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -18,6 +18,16 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['method'])) {
     }
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['method'])) {
+    $method = $_POST['method'];
+    if(method_exists('ProductController', $method)) {
+        $product = new ProductController();
+        $product->$method($_POST);
+    } else {
+        echo 'Metodo incorreto';
+    }
+}
+
 class ProductController{
 
     public function index(){
@@ -26,6 +36,71 @@ class ProductController{
 
         echo json_encode($products);
 
+    }
+
+    public function store(){
+        $msgError = "";
+
+        if(empty($_POST['name'])){
+            $msgError = "<li>O campo Nome deve ser preenchido</li>";
+        }
+
+        if(empty($_POST['description'])){
+            $msgError .= "<li>O campo Descrição deve ser preenchido</li>";
+        }
+
+        if(empty($_POST['price'])){
+            $msgError .= "<li>O campo Preço deve ser preenchido</li>";
+        }
+        if(!empty($msgError)){
+            echo json_encode(['msg'=>$msgError]);
+        }
+
+
+
+        if(empty($msgError)){
+            $product = new Product();
+            $product->setName($_POST['name']);
+            $product->setDescription($_POST['description']);
+            $product->setPrice($_POST['price']);
+            $product->create();
+            $product = $product->findLast();
+            echo json_encode([$product]);
+        }
+    }
+
+    public function show(){
+        $product = new Product();
+        $product = $product->find($_GET['id']);
+        echo json_encode($product);
+    }
+
+    public function update(){
+        $msgError = "";
+
+        if(empty($_POST['name'])){
+            $msgError = "<li>O campo Nome deve ser preenchido</li>";
+        }
+
+        if(empty($_POST['description'])){
+            $msgError .= "<li>O campo Descrição deve ser preenchido</li>";
+        }
+
+        if(empty($_POST['price'])){
+            $msgError .= "<li>O campo Preço deve ser preenchido</li>";
+        }
+
+        echo json_encode(['msg'=>$msgError]);
+
+
+
+        if(empty($msgError)){
+            $product = new Product();
+            $product->setName($_POST['name']);
+            $product->setDescription($_POST['description']);
+            $product->setPrice($_POST['price']);
+            $product->update($_POST['id']);
+        }
     }
 
 }
--- View/assets/js/index.js
+++ View/assets/js/index.js
@@ -1,25 +1,53 @@
-$(document).ready(function() {
-
-
+$(document).ready(function () {
     $.ajax({
-        url: 'http://localhost/final/Controller/ProductController.php?method=index',
+        url: 'http://localhost:81/final/Controller/ProductController.php?method=index',
         dataType: 'json'
     }).done(function (data) {
 
         var i;
         for (i = 0; i < data.length; i++) {
-            products = "<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].description+"</td><td>"+data[i].price+"</td>";
-            products += "<td><a href='' class='btn btn-primary'>Editar </a> ";
-            products += "<a href='' class='btn btn-danger'>Excluir</a></td></tr>";
+            products = "<tr><td>" + data[i].id + "</td><td>" + data[i].name + "</td><td>" + data[i].description + "</td><td>" + data[i].price + "</td>";
+            products += "<td><a href='../../../../final/View/product/create-update.html?id=" + data[i].id + "' class='btn btn-primary'>Editar </a> ";
+            products += "<a href='http://localhost:81/final/Controller/ProductController.php?method=destroy&id=" + data[i].id + "' class='btn btn-danger'>Excluir</a></td></tr>";
             $('#product-list').append(products);
+
         }
-
-
-
     });
 });
 
-function saveProduct(){
+
+$(document).ready(function () {
+    var query = location.search.slice(1);
+    var partes = query.split('&');
+    var data = {};
+    partes.forEach(function (parte) {
+        var chaveValor = parte.split('=');
+        var chave = chaveValor[0];
+        var valor = chaveValor[1];
+        data[chave] = valor;
+    });
+
+    $.ajax({
+        url: 'http://localhost:81/final/Controller/ProductController.php?method=show&id=' + data.id,
+        dataType: 'json'
+    }).done(function (data) {
+        $('#name').val(data.name);
+        $('#description').val(data.description);
+        $('#price').val(data.price);
+
+        if (data.id) {
+            btnupdate = '<button type="button" id="save" name="save" class="btn btn-primary" onclick="updateProduct()">Atualizar</button>';
+            id = '<input id="id" name="id" type="hidden" value="' + data.id + '">';
+            $('#save').remove();
+            $('#btn-salvar').append(btnupdate);
+            $('#field').append(id);
+            $('#method').val('update');
+        }
+    });
+});
+
+
+function saveProduct() {
     var data = $('#form-product').serialize();
 
     $.ajax({
@@ -28,29 +56,24 @@ function saveProduct(){
         data: data,
         dataType: 'json'
     }).done(function (data) {
-        if (!data.msg == ""){
-            $('.display-error').html('<ul>'+data.msg+'</ul>');
+        if (!data.msg == "") {
+            $('.display-error').html('<ul>' + data.msg + '</ul>');
             $('.display-error').css('display', 'block');
             $('.display-success').hide();
 
         }
 
-        else{
+        else {
             console.log(data);
             $('.display-success').html('<ul><li>Produto Cadastrado</li></ul>');
             $('.display-success').css('display', 'block');
             $('.display-error').hide();
-
-            product = '<tr><th scope="row">' + data[0].id + '</th><td>' + data[0].name + '</td><td>' + data[0].description + '</td><td>'+data[0].price+'</td>';
-            product += '<td><a href="update-product.php?id='+data[0].id+'" class="btn btn-primary">Editar</a></td></tr>';
-
-            $('#product-list').append(product);
         }
     })
 }
 
 function updateProduct() {
-    var data = $('#form-product-update').serialize();
+    var data = $('#form-product').serialize();
 
     $.ajax({
         url: '../../Controller/ProductController.php',
@@ -58,13 +81,13 @@ function updateProduct() {
         data: data,
         dataType: 'json',
     }).done(function (data) {
-        if (!data.msg == ""){
-            $('.display-error').html('<ul>'+data.msg+'</ul>');
+        if (!data.msg == "") {
+            $('.display-error').html('<ul>' + data.msg + '</ul>');
             $('.display-error').css('display', 'block');
             $('.display-success').hide();
         }
 
-        else{
+        else {
             console.log(data);
             $('.display-success').html('<ul><li>Produto Atualizado</li></ul>');
             $('.display-success').css('display', 'block');
@@ -72,4 +95,5 @@ function updateProduct() {
         }
     })
 }
+
 
